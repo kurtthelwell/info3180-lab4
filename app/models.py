8c0fea7b8eceb4315706e36a3d90e49e8699411e
Kurt <kurtwilliam28@gmail.com>
2026-03-17 02:41:34 -0500

    Added password field

--- app/models.py
+++ app/models.py
@@ -6,6 +6,29 @@ class UserProfile(db.Model):
     # the class name. In this case a class name of UserProfile would create a
     # user_profile (singular) table, but if we specify __tablename__ we can change it
     # to `user_profiles` (plural) or some other name.
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(128))  # ADD THIS
+
+    def __init__(self, first_name, last_name, username, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = generate_password_hash(password, method='pbkdf2:sha256')
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
     __tablename__ = 'user_profiles'
 
     id = db.Column(db.Integer, primary_key=True)
